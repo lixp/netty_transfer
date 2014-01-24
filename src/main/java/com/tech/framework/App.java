8226ca317b7d716a12e2c4e858d5800983583901
lixp <po_lee@msn.cn>
2014-01-25 02:34:16 +0800

    add app

---
+++ src/main/java/com/tech/framework/App.java
@@ -0,0 +1,13 @@
+package com.tech.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!123" );
+    }
+}
